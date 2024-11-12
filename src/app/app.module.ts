8fb8297d344b14d0f65312187655b908de16e5c6
Sofia Toro <laura.toro01@uceva.edu.co>
2024-11-12 15:07:26 -0500

    Se implenta la logica para editar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarFacultadComponent } from './facultad/listar-facultad/listar-facultad.component';
+import { EditarFacultadComponent } from './facultad/editar-facultad/editar-facultad.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    ListarFacultadComponent
+    ListarFacultadComponent,
+    EditarFacultadComponent
   ],
   imports: [
     BrowserModule,
