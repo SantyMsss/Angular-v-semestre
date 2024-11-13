ba2bdd74c9f44fe633777f5a87888442b8ef15ea
Angie Lorena González <angie.gonzales01@uceva.edu.co>
2024-11-12 22:50:42 -0500

    Se añade el componente detalle-facultad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListarFacultadComponent } from './facultad/listar-facultad/listar-facultad.component';
+import { DetalleFacultadComponent } from './facultad/detalle-facultad/detalle-facultad.component';
 
 @NgModule({
   declarations: [
     AppComponent
-    ListarFacultadComponent
+    ListarFacultadComponent,
+    DetalleFacultadComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/facultad/detalle-facultad/detalle-facultad.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import {Facultad} from "../model/facultad";
+
+@Component({
+  selector: 'app-detalle-facultad',
+  templateUrl: './detalle-facultad.component.html',
+  styleUrls: ['./detalle-facultad.component.css']
+})
+export class DetalleFacultadComponent {
+  @Input() facultad! Facultad;
+}
